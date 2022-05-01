29cbe8c025a6e45024126f9ab9a55eaa86dde20f
danielhillsman <daniel.loves.junk@gmail.com>
2022-04-30 19:05:58 -0700

    fixed server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,9 @@
 // Requires all the packages 
 const express = require("express");
 const routes = require("./routes");
-
-
-
 const app = express();
-app.use(express.static("public"));
 
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
