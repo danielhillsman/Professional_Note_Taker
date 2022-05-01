37922ba9a58d4faa1705454cce2e189ee3216522
danielhillsman <daniel.loves.junk@gmail.com>
2022-04-30 19:48:03 -0700

    took away what i wrote on server.js and created a new file and a dir named config

--- Develop/server.js
+++ Develop/server.js
@@ -3,21 +3,6 @@ const express = require("express");
 const routes = require("./routes");
 const app = express();
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: "localhost",
-      dialect: "mysql",
-      port: 3306,
-    }
-  );
-}
-
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
